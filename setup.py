6991013088f64c2f1dfbfac8d20b817d6cd99b3c
Sam Boesen <sam.boesen2@gmail.com>
2024-12-28 10:47:02 -0700

    Update setup.py and requirements.txt for cloud deployment

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="biotech_sentiment",
     version="0.1",
-    packages=find_packages(),
+    packages=find_packages(include=['biotech_sentiment', 'biotech_sentiment.*']),
     install_requires=[
         'streamlit',
         'plotly',
@@ -12,5 +12,7 @@ setup(
         'textblob',
         'newsapi-python',
         'gnews',
+        'nltk'
     ],
+    python_requires='>=3.7',
 ) 
